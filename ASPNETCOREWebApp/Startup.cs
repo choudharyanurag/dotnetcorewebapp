09c31e856a3b83590c7144a775ffb59a9ad4dc42
choudharyanurag <33773725+choudharyanurag@users.noreply.github.com>
2018-07-16 15:19:28 +0530

    Update Startup.cs

--- ASPNETCOREWebApp/Startup.cs
+++ ASPNETCOREWebApp/Startup.cs
@@ -29,7 +29,7 @@ namespace ASPNETCOREWebApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello CD!");
             });
         }
     }
